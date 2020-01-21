972bbd28e9911279b265830ea223a0aee8e58267
Bersder3000 <B1747322151@outlook.com>
2020-01-21 19:09:16 +0800

    SP for coding(Dynamic*)

--- src/components/DynamicCard.vue
+++ src/components/DynamicCard.vue
@@ -1,15 +1,129 @@
 <template>
     <div class="d-card">
-		<div class="author-avatar"></div>
+		<div class="author-avatar" :style="{backgroundImage:`url(${ddata.avatar})`}"></div>
 		<div class="content tl">
-			<p class="author-namae">我的世界</p>
-			<p class="time">刚刚</p>
+			<p class="author-namae">{{ddata.author}}</p>
+			<p class="time">{{ddata.time|commentTime}}</p>
 			<div class="d-card-content">
-				<div class="text" v-html="dRenderer(test,0,0)"></div>
-				<div class="imagesbox"></div>
+				<div class="text" v-html="dRenderer(ddata,0,0)" v-highlight></div>
+				<!--当存在图片时开启下面的imgsbox-->
+				<div class="imgsbox no-select" v-if="ddata.imgs.length">
+					<div class="thumbnail-wrap" v-if="!zoomingIn">
+						<ul class="thumbnail-list fzz">
+							<li class="item" v-for="(item,index) in ddata.imgs">
+								<div class="img-content" :style="{backgroundImage:`url(${item})`}"></div>
+							</li>
+						</ul>
+					</div>
+					<div class="zoom-wrap" v-else>
+						<ul class="zoom-control">
+							<li><i class="iconfont icon-compress"></i><span>收起</span></li>
+							<li><i class="iconfont icon-expand"></i><span>大图</span></li>
+							<li><i class="iconfont icon-rotateleft"></i><span>向左旋转</span></li>
+							<li><i class="iconfont icon-rotateright"></i><span>向右旋转</span></li>
+						</ul>
+						<div class="zoom-img">
+							<div class="zoom-img-container"><img :src="ddata.imgs[zoomingIndex]"></div>
+							<div class="prev"></div>
+							<div class="next"></div>
+						</div>
+						<!--当不止一张图片时才启用滑动栏-->
+						<div class="zoom-slider-wrap" v-if="ddata.imgs.length>1">
+							<div class="zoom-slider-border" :style="{left:`${zoomingIndex*66}px`}"></div>
+							<ul class="zoom-slider fzz">
+								<li v-for="(item,index) in ddata.imgs"><img :src="item"></li>
+							</ul>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="d-card-bar">
-
+			<div class="d-card-btn-bar">
+				<button><i class="iconfont icon-comment"></i>{{ddata.commentCount}}</button>
+				<button><i class="iconfont icon-heart"></i>{{ddata.liked}}</button>
+			</div>
+		</div>
+		<div class="bottom">
+			<div class="d-card-comment">
+				<div id="respond" class="respond">
+					<button class="cancel-reply-btn" v-if="to_id" @click="cancelReply">取消回复</button>
+					<h3 class="respond-title">{{to_uname|respondTitle}}<span v-show="to_id&&!isMobile"> | #{{to_id}}</span></h3>
+					<div class="comment-form">
+						<div class="comment-info-input">
+							<input placeholder="昵称(必填)" v-model.trim="nickname" name="nickname">
+							<input placeholder="邮箱(必填，保密)" v-model.trim="email" name="email">
+							<input placeholder="qq(选填，拉取头像)" v-model.trim="qq" name="qqnum">
+						</div>
+						<span title="除了html、标题、分割线、表格、图片、下划线、标记、上下标"><i class="iconfont icon-markdown"></i>Markdown Supported</span>
+						<div class="comment-content-input"><textarea @keydown="textareaTab" placeholder="说点什么吧..." v-model="content"></textarea></div>
+					</div>
+					<div class="comment-buttons tr">
+						<div class="emotion pl" :class="{'emo-open':emoBoxShow}">
+							<span class="emotion-toggle" @click="emoBoxShow=!emoBoxShow"><i class="iconfont icon-emoji ibold"></i>表情</span>
+							<div class="emotion-box">
+								<div class="emo-title"><span>{{emoData[emoIndex].emoSeries}}</span></div>
+								<div class="emo-wrap" :class="{'emo-text':!emoData[emoIndex].pic}">
+									<a v-for="item in emoData[emoIndex].emoList"
+									   :title="item.des"
+									   @click="insertEmo(item,emoData[emoIndex].pic)"><img v-if="emoData[emoIndex].pic" :src="'/root'+item.imgSrc" :alt="item.des"><span v-else>{{item}}</span></a>
+								</div>
+								<div class="emo-tabs">
+									<a v-for="(item,index) in emoData" :key="index" @click="emoIndex=index" :class="{cur:index===emoIndex}"><img :src="'/root'+item.thumbnail" :alt="item.emoSeries" height="22" width="22"></a>
+								</div>
+							</div>
+						</div>
+						<span><label><input type="checkbox" v-model="notifyMe"> 回复提醒</label></span>
+						<button @click="commentSubmit">提交评论</button>
+					</div>
+				</div>
+				<div class="comments-main" id="anchor">
+					<h3 class="comments-list-title">Comments<span> | {{allCount}} 条评论</span></h3>
+					<div class="waiting" v-if="commentWaiting">
+						<div class="rect1"></div>
+						<div class="rect2"></div>
+						<div class="rect3"></div>
+						<div class="rect4"></div>
+						<div class="rect5"></div>
+					</div>
+					<div class="comments-list" v-else-if="allCount">
+						<div class="comments-list-item" :id="'comment-'+comment.id" v-for="comment in commentList" :key="comment.id">
+							<div class="comment-content-wrap">
+								<div class="comment-avatar">
+									<img src="/static/images/spinner-preloader.svg" class="lazyload" :data-src="comment.avatar||'/static/images/passerby.png'">
+								</div>
+								<div class="comment-meta">
+									<p class="uname"><a :href="comment.ulink">{{comment.uname}}</a></p>
+									<span class="comment-time" :title="comment.datetime.substr(0,16)">{{comment.datetime|commentTime}}</span>
+									<span v-show="!isMobile" class="comment-id">#{{comment.id}}</span>
+								</div>
+								<div class="comment-content" v-html="dRenderer(comment,1,0)" v-highlight>
+								</div>
+								<button class="comment-reply no-select" @click="replyThis(comment.id,comment.uid,comment.uname)">回复</button>
+							</div>
+							<div class="comment-children">
+								<div class="comments-list">
+									<div class="comments-list-item" :id="'comment-'+reply.id" v-for="reply in comment.children" :key="reply.id">
+										<div class="comment-content-wrap">
+											<div class="comment-avatar">
+												<img src="/static/images/spinner-preloader.svg" class="lazyload" :data-src="reply.avatar||'/static/images/passerby.png'">
+											</div>
+											<div class="comment-meta">
+												<p class="uname"><a :href="reply.ulink">{{reply.uname}}</a><span v-show="!isMobile"><span style="font-weight: normal"> 回复 </span><span>@{{reply.to_uname}}<span class="reply-id"> | #{{reply.parent_id}}</span></span></span></p>
+												<span class="comment-time" :title="comment.datetime.substr(0,16)">{{reply.datetime|commentTime}}</span>
+												<span v-show="!isMobile" class="comment-id">#{{reply.id}}</span>
+											</div>
+											<div class="comment-content" v-html="dRenderer(reply,1,1)" v-highlight>
+											</div>
+											<button class="comment-reply no-select" @click="replyThis(reply.id,reply.uid,reply.uname)">回复</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="no-comment" v-else>
+						<h3>这里什么都没有`╮(￣▽￣)╭` </h3>
+					</div>
+				</div>
 			</div>
 
 		</div>
@@ -18,8 +132,7 @@
 
 <script>
 	import marked from 'marked';
-	import {unique} from "../utils/lib";
-
+	import richTextMixin from "../mixins/Mixin-RichText";
 	export default {
         name: "DynamicCard",
 		async created(){
@@ -40,40 +153,14 @@
 				smartypants: false
 			});
 			await this.fetchEmo();
-			this.test.content = '**wwww** ∫f(2233_无语)'
 		},
 		data(){
         	return{
-				emoData:[{emoSeries:'',emoList:[]}],
-				emoMap:{},
-				emoBoxShow:false,
-				emoIndex:0,
-
-				zooming:false,
-        		commentList: [],
-				test:{content:'**wwww**'}
+				zoomingIn:1,
+				zoomingIndex:0,
 			}
 		},
 		methods:{
-			async fetchEmo(){
-				let response = await this.$fetch('/static/emo/emo.json');
-				let data = response.data;
-				data.forEach(e=>{
-					e.thumbnail = e.path + e.thumbnail;
-					if (e.pic){
-						let f = e.path.split('/');
-						let g = f[f.length-2]+'_';//类别前缀
-						e.emoList.forEach(f=>{
-							f.des = g + f.des;
-							f.imgSrc=e.path+f.imgSrc;
-							f.insert = '∫f(' + f.des + ')';
-							this.emoMap[f.des] = f.imgSrc;
-						});
-					}
-				});
-				this.emoData = data;
-				console.log('1111')
-			},
         	dRenderer(item,isComment,isReply){
         		if (isComment){
 					let start = (isReply&&this.isMobile)?`[@${item.to_uname}](#dc-${item.parent_id})`:'';
@@ -82,78 +169,37 @@
         		else
         			return marked(this.emoRenderer(item.content))
 			},
-			emoRenderer(raw){
-				let tmp = raw;
-				let replaceSrcList = tmp.match(/∫f\(.+?\)/g);
-				if (replaceSrcList){ //当使用了表情包后，需要转义替换以便mark
-					let replaceDstList = [];
-					let replaceAltList = [];
-					replaceSrcList = unique(replaceSrcList);
-					replaceSrcList.forEach(e=>{
-						let alt = e.match(/∫f\((.+?)\)/)[1];
-						replaceAltList.push(alt);
-						console.log(alt);
-						replaceDstList.push(this.emoMap[alt]);
-						console.log(this.emoMap[alt])
-					});
-					// console.log(replaceSrcList);
-					// console.log(replaceAltList);
-					// console.log(replaceDstList);
-					for (let i = 0; i < replaceSrcList.length; i++){
-						let reg = new RegExp(replaceSrcList[i].replace('(','\\(').replace(')','\\)'),'g');
-						if (replaceDstList[i])//没有的表情不进行渲染
-							tmp = tmp.replace(reg,'!['+replaceAltList[i]+'](/root'+replaceDstList[i]+') ');
-						else
-							tmp = tmp.replace(reg,' ');
-					}
-				}
-				return tmp
-			},
 			commentToggle(){
+
+			},
+			imgRotate(direction){
+
+			},
+			fetchComment(offset){
+				this.$fetch('/apis/apiv16p1.php',{did:this.did,offset:offset}).then(response=>{
+					if (response.data.code < 1) {
+						let data = response.data.data;
+						this.allCount = data.allCount;
+						this.pageNum = Math.ceil(data.commentCount/10);
+						//console.log(this.allCount,this.pageNum);
+						this.commentWaiting = false;
+						data.comments.forEach(e=>{
+							e['children'] = this.gen_children(e);
+							delete e.replies;
+							this.commentList.push(e)
+						});
+						setTimeout(()=>this.$store.commit('lazyCheck'),100);
+					}
+				})
+			},
+			commentSubmit(){
 
 			}
 		},
-		computed:{
-			isMobile(){return this.$store.state.isMobile}
-		},
-		props:['ddata']
+		props:['ddata'],
+		mixins:[richTextMixin]
     }
 </script>
 
 <style>
-	.d-card{
-		position: relative;
-		border: .01rem solid #eaeaea;
-		border-radius: .04rem;
-		background: #fcfaf2;
-	}
-	.d-card .author-avatar{
-		position: absolute;
-		width: .5rem;
-		height: .5rem;
-		top: .2rem;
-		left: .2rem;
-		background: url("http://localhost/uploads/avatar/me.png") center/cover no-repeat;
-		border-radius: 50%;
-	}
-	.d-card .content{
-		margin-left: .8rem;
-		margin-right: .1rem;
-		padding-top: .3rem;
-		height: 5rem;/*tmp*/
-	}
-		.d-card .content .time{
-			font-size: .12rem;
-			color: #bbb;
-		}
-		.d-card-content{
-			margin-top: .1rem;
-			padding-right: .1rem;
-		}
-			.d-card-content .text{
-				margin-bottom: .1rem;
-				font-size: .14rem;
-				word-wrap: break-word;
-			}
-
 </style>
--- src/views/Dynamic.vue
+++ src/views/Dynamic.vue
@@ -8,16 +8,16 @@
 						<div class="content tl" v-else>
 							<div class="author-pic"></div>
 							<p class="fzz">
-								<router-link to="/about" class="author-namae">忍野喵</router-link>
+								<router-link to="/about" class="author-namae">{{authorInfo.author}}</router-link>
 								<span class="author-status no-select" :style="{background:statusMap[status].color}"><i class="iconfont" :class="statusMap[status].icon"></i>{{statusMap[status].des}}</span>
-								<span class="author-sign">我的世界我的世界我的世界我的世界我的世界我的世界界我的世界我的世界</span>
+								<span class="author-sign">{{authorInfo.signature}}</span>
 							</p>
 							<div class="author-statistics tc">
-								<router-link to="/archive" class="cell"><p>233</p><span>文章</span></router-link>
-								<router-link to="/note" class="cell"><p>233</p><span>笔记</span></router-link>
-								<router-link to="" class="cell"><p>233</p><span>动态</span></router-link>
+								<router-link to="/archive" class="cell"><p>{{statistics.aCount}}</p><span>文章</span></router-link>
+								<router-link to="/note" class="cell"><p>{{statistics.nCount}}</p><span>笔记</span></router-link>
+								<router-link to="" class="cell"><p>{{statistics.dCount}}</p><span>动态</span></router-link>
 							</div>
-							<span class="author-avatar"><img alt="author" src="http://localhost/uploads/avatar/me.png"></span>
+							<span class="author-avatar"><img alt="author" :src="authorInfo.avatar"></span>
 						</div>
 					</div>
 
@@ -25,7 +25,7 @@
 						<p class="title">推荐所</p>
 						<div class="loading-content" v-if="loading"></div>
 						<div class="recommend-list" v-else>
-							<a target="_blank" href="https://baidu.com">推荐y</a>
+							<a v-for="(item,index) in recommendations" target="_blank" :key="index" :href="item.url">{{item.title}}</a>
 						</div>
 					</div>
 				</div>
@@ -42,10 +42,9 @@
 					</div>
 					<div class="main-panel">
 						<div class="d-card-list">
-							<dynamic-card></dynamic-card>
+							<dynamic-card v-for="item in dynamics" :key="item.id" :ddata="item"></dynamic-card>
 						</div>
 					</div>
-
 				</div>
 			</div>
 		</div>
@@ -53,26 +52,61 @@
 </template>
 
 <script>
+	import {mapGetters} from 'vuex';
 	import UCONF from "../config/user.conf";
 	import DC from '../components/DynamicCard'
     export default {
         name: "Dynamic",
 		data(){
         	return{
-        		loading:false,
+        		loading:true,//标识初始化加载状态
 				status:0,
 				statusMap:UCONF.statusMap,
 				authorInfo:{},
+				statistics:{},
 				recommendations:[],
 				danmaku:'',
-				dynamics:[]
+
+				dynamics:[],
+				waiting:false,//标识更多动态加载状态
+				noMore:false//标识是否穷尽动态
 			}
 		},
-		async created(){
-
+		created(){
+        	this.$fetch('/apis/apiv16.php').then(response=>{
+				if (response.data.code < 1) {
+					let data = response.data.data;
+					this.loading = false;
+					this.authorInfo.author = data.author;
+					this.authorInfo.avatar = data.avatar;
+					this.authorInfo.signature = data.info.sign;
+					this.status = data.info.status;
+					this.statistics = data.statistics;
+					data.recommendations.forEach(e=>this.recommendations.push(e));
+					data.dynamics.forEach(e=>this.dynamics.push(e));
+				}
+			});
+		},
+		watch:{
+        	reachBottom(cur,pre){
+        		if (cur) this.loadMore();
+			}
 		},
 		methods:{
-
+			loadMore(){
+				if (!this.noMore&&!this.waiting) {
+					this.waiting = true;
+					this.$fetch('/apis/apiv16.php',{offset:this.dynamics.length}).then(response=>{
+						let tmp = response.data.data.dynamics;
+						this.waiting = false;
+						if (tmp.length) tmp.forEach(e=>this.dynamics.push(e));
+						else this.noMore = true;
+					})
+				}
+			}
+		},
+		computed:{
+			...mapGetters(['reachBottom']),
 		},
 		components:{
         	'dynamic-card':DC,
